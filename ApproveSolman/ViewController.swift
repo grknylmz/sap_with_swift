7b1a78e285ebe99b07e0b06a524c0fb4e72f41b5
Gurkan Yilmaz <gurkanyilmaz@gurkans-macbook-pro.local>
2016-08-10 14:22:22 +0300

    Initial Commit

---
+++ ApproveSolman/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ApproveSolman
+//
+//  Created by Gurkan Yilmaz on 10/08/16.
+//  Copyright © 2016 Gurkan Yilmaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
